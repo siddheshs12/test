160f3d08f329147e2c42b3819ff4f68ffacb9caf
Ujjwal <ujjwal@neuralbits.com>
2026-02-09 19:00:47 +0530

    landing page by ujjwal

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import Register from "./pages/Register";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import Register from './pages/Register'; // Using the file visible in your screenshot
+
+// Placeholder for Login if you don't have it yet
+const Login = () => <div className="text-center mt-20">Login Page Coming Soon</div>;
 
 function App() {
-  return <Register />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
